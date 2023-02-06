13db6e7235bff1bd0019e1b23340c7b41c7127c4
TechnoDotΩ <73961295+TechDudie@users.noreply.github.com>
2023-02-06 17:43:12 +0000

    lets goo!!!

--- polynomial.py
+++ polynomial.py
@@ -7,13 +7,24 @@ from numpy.linalg import inv
 
 getcontext().prec = 50
 
-x_axis = np.array([0, 0.5, 1, 1.5, 2, 2.3, 2.7, 3, 3.3, 3.6, 4, 4.7, 5, 5.5, 6, 6.5, 6.8, 7, 7.4, 7.8, 7.9, 8, 8.2, 8.5, 8.9, 9, 9.4, 9.8, 10])
-y_axis = np.array([0, 0.25, 0.5, 0.75, 1, 0.8, 1.2, 1, 2, 3, 2.6, 2.25, 2.5, 3, 3.4, 4, 4.3, 4.2, 4, 3.9, 4, 4.5, 5, 4.6, 4.25, 5, 5.6, 6.2, 6.1])
+x_axis = np.array([0, 0.5, 1, 1.5, 2, 2.3, 2.7, 3, 3.3, 3.6, 4, 4.7, 5, 5.5, 6, 6.5, 6.8, 7, 7.4, 7.8, 7.9, 8, 8.2, 8.5, 8.9, 9, 9.4, 9.8, 10]) # type: ignore
+y_axis = np.array([0, 0.25, 0.5, 0.75, 1, 0.8, 1.2, 1, 2, 3, 2.6, 2.25, 2.5, 3, 3.4, 4, 4.3, 4.2, 4, 3.9, 4, 4.5, 5, 4.6, 4.25, 5, 5.6, 6.2, 6.1]) # type: ignore
 
 def polynomial(x_axis, y_axis, power):
+    """
+    Calculates a polynomial approximation from a list of points and a given polynomal degree.
+
+    Parameters:
+        x_axis (list): A list of x-values
+        y_axis (list): A list of y-values
+
+    Returns:
+        coefficients (list): A list of polynomial coefficients, in descending order
+    """
     x = np.empty((len(x_axis), power + 1))
     for i, j in enumerate(x):
-        for j in range(power + 1): x[i, j] = x_axis[i] ** j
+        for j in range(power + 1):
+            x[i, j] = x_axis[i] ** j
     xt = transpose(x)
     b = np.dot(np.dot(inv(np.dot(xt, x)), xt), y_axis)
     x = 0
@@ -24,17 +35,36 @@ def polynomial(x_axis, y_axis, power):
     return coefficients
 
 def derivative(coefficients):
+    """
+    Calculates the derative of a list of coefficients.
+
+    Parameters:
+        coefficients (list): A list of polynomial coefficients, in descending order
+
+    Returns:
+        result (list): The derative of the coefficients
+    """
     result = []
     coefficients = coefficients[::-1]
     x = -1
     for i in coefficients:
         x += 1
-        if x == 0: continue
+        if x == 0:
+            continue
         result.append(i * x)
     result = result[::-1]
     return result
 
 def peak(y_axis):
+    """
+    Calculates how many times a graph peaks.
+
+    Parameters:
+        y_axis (list): A list of y-values
+
+    Returns:
+        counter (int): The amount of times the graph peaks
+    """
     state = True
     prev = 0
     counter = 1
@@ -51,9 +81,16 @@ def peak(y_axis):
     return counter // 2
 
 def polyprint(coefficients, desmos=False):
+    """
+    Prints a list of coefficients in polynomial standard form.
+
+    Parameters:
+        coefficients (list): A list of polynomial coefficients, in descending order
+        desmos (bool): A flag whether or not to print exponents in Desmos form
+    """
     strings = []
     for i in coefficients:
-        if strings == []:
+        if not strings:
             if i > 0:
                 strings.append('{0:.30f}'.format(abs(i)))
             elif i < 0:
@@ -77,6 +114,16 @@ def polyprint(coefficients, desmos=False):
     print(string)
 
 def polygraph(x, coefficients):
+    """
+    Graphs a list of coefficients in polynomial standard form.
+
+    Parameters:
+        x (int): The x-value
+        coefficients (list): A list of polynomial coefficients, in descending order
+
+    Returns:
+        y (int): The resulting y-value
+    """
     coefficients = coefficients[::-1]
     degree = len(coefficients) - 1
     y = 0
@@ -84,19 +131,20 @@ def polygraph(x, coefficients):
         y += coefficients[i] * x ** i
     return y
 
-power = peak(y_axis)
-print(power)
-coefficients = polynomial(x_axis, y_axis, power)
-print(coefficients)
-polyprint(coefficients)
-derivative_1 = derivative(coefficients)
-polyprint(derivative_1)
-derivative_2 = derivative(derivative_1)
-polyprint(derivative_2)
+if __name__ == "__main__":
+    power = peak(y_axis)
+    print(power)
+    coeffs = polynomial(x_axis, y_axis, power)
+    print(coeffs)
+    polyprint(coeffs)
+    derivative_1 = derivative(coeffs)
+    polyprint(derivative_1)
+    derivative_2 = derivative(derivative_1)
+    polyprint(derivative_2)
 
-x = np.linspace(0, 10, 100)
-plt.scatter(x_axis, y_axis, color="black")
-plt.plot(x, polygraph(x, coefficients), color="red")
-plt.plot(x, polygraph(x, derivative_1), color="green")
-plt.plot(x, polygraph(x, derivative_2), color="blue")
-plt.show()
+    x_linspace = np.linspace(0, 10, 100)
+    plt.scatter(x_axis, y_axis, color="black")
+    plt.plot(x_linspace, polygraph(x_linspace, coeffs), color="red")
+    plt.plot(x_linspace, polygraph(x_linspace, derivative_1), color="green")
+    plt.plot(x_linspace, polygraph(x_linspace, derivative_2), color="blue")
+    plt.show()
