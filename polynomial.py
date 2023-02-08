bd76103f060ff30a76d9af1af91b87172537d94a
TechnoDotΩ <73961295+TechDudie@users.noreply.github.com>
2023-02-07 20:36:02 -0500

    Update polynomial.py

--- polynomial.py
+++ polynomial.py
@@ -1,14 +1,53 @@
 from decimal import Decimal, getcontext
-
-import matplotlib.pyplot as plt
 import numpy as np
 from numpy import transpose
 from numpy.linalg import inv
+import matplotlib.pyplot as plt
+from pandas_datareader import data as reader
+import yfinance as yf
+from datetime import datetime
+import pytz
+import math
+import socket
+import sys
 
+def log(message, level="INFO"):
+    now = datetime.now().strftime("[%H:%M:%S]")
+    print(f"{now} [{level}] {message}")
+
+time = lambda: (datetime.utcnow() - datetime(1970, 1, 1)).total_seconds() * 1000
+
+ticker = "MSFT"
+desmos = False
 getcontext().prec = 50
+yf.pdr_override()
 
-x_axis = np.array([0, 0.5, 1, 1.5, 2, 2.3, 2.7, 3, 3.3, 3.6, 4, 4.7, 5, 5.5, 6, 6.5, 6.8, 7, 7.4, 7.8, 7.9, 8, 8.2, 8.5, 8.9, 9, 9.4, 9.8, 10]) # type: ignore
-y_axis = np.array([0, 0.25, 0.5, 0.75, 1, 0.8, 1.2, 1, 2, 3, 2.6, 2.25, 2.5, 3, 3.4, 4, 4.3, 4.2, 4, 3.9, 4, 4.5, 5, 4.6, 4.25, 5, 5.6, 6.2, 6.1]) # type: ignore
+log("Modules imported and initialized")
+
+if len(sys.argv) > 1:
+    ticker = sys.argv[1]
+    log(f"Ticker set. Target: {ticker}")
+else:
+    log("Ticker not specified. Defaulting to 'MSFT'", level="WARN")
+
+if len(sys.argv) > 2:
+    desmos = True
+    log("Desmos mode enabled. Polynomial exponents will be printed in Desmos format")
+else:
+    log("Desmos mode disabled. Polynomial exponents will be printed with a caret.")
+
+try:
+    socket.setdefaulttimeout(3)
+    socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(("8.8.8.8", 53))
+except socket.error:
+    log("No internet. Please connect to internet, and try again.", level="ERROR")
+    exit()
+
+now = datetime.now(pytz.timezone('US/Eastern'))
+if (now.hour == 9 and now.minute >= 30) or (now.hour > 9 and now.hour <= 16):
+    log(f"Stock market open. Eastern time is {now.strftime('%H:%M:%S')}")
+else:
+    log("Stock market is closed. Retreiving most recent values.", level="WARN")
 
 def polynomial(x_axis, y_axis, power):
     """
@@ -80,6 +119,19 @@ def peak(y_axis):
         prev = i
     return counter // 2
 
+def degree(y_axis):
+    """
+    Calculates the optimal polynomial degree from a set of points.
+
+    Parameters:
+        y_axis (list): A list of y-values
+
+    Returns:
+        degree (int): The optimal degree.
+    """
+    # I'll be honest, I just made this random algorition up.
+    return math.floor(math.e * math.log(len(y_axis)))
+
 def polyprint(coefficients, desmos=False):
     """
     Prints a list of coefficients in polynomial standard form.
@@ -92,14 +144,14 @@ def polyprint(coefficients, desmos=False):
     for i in coefficients:
         if not strings:
             if i > 0:
-                strings.append('{0:.30f}'.format(abs(i)))
+                strings.append('{0:.5f}'.format(abs(i)))
             elif i < 0:
-                strings.append("-" + '{0:.30f}'.format(abs(i)))
+                strings.append("-" + '{0:.5f}'.format(abs(i)))
             continue
         if i > 0:
-            strings.append(" + " + '{0:.30f}'.format(abs(i)))
+            strings.append(" + " + '{0:.5f}'.format(abs(i)))
         elif i < 0:
-            strings.append(" - " + '{0:.30f}'.format(abs(i)))
+            strings.append(" - " + '{0:.5f}'.format(abs(i)))
     string = "y = "
     x = len(strings)
     for i in strings:
@@ -122,7 +174,7 @@ def polygraph(x, coefficients):
         coefficients (list): A list of polynomial coefficients, in descending order
 
     Returns:
-        y (int): The resulting y-value
+        y (float): The resulting y-value
     """
     coefficients = coefficients[::-1]
     degree = len(coefficients) - 1
@@ -132,19 +184,38 @@ def polygraph(x, coefficients):
     return y
 
 if __name__ == "__main__":
-    power = peak(y_axis)
-    print(power)
-    coeffs = polynomial(x_axis, y_axis, power)
-    print(coeffs)
-    polyprint(coeffs)
-    derivative_1 = derivative(coeffs)
-    polyprint(derivative_1)
-    derivative_2 = derivative(derivative_1)
-    polyprint(derivative_2)
+    ms = time()
 
-    x_linspace = np.linspace(0, 10, 100)
+    # Download the stock data from Yahoo Finance
+    log("Downloading stock data...")
+    data = list(reader.get_data_yahoo(ticker, period="1d", interval="1m")["Close"].values[-5:])
+    print(data)
+    x_axis = []
+    y_axis = []
+    for x, y in enumerate(data):
+        x_axis.append(x)
+        y_axis.append(y)
+    print(data)
+
+    log("Approximating polynomial...")
+    print(degree(y_axis))
+    coeffs = polynomial(x_axis, y_axis, degree(y_axis)) # Calculate the polynomial approximation from the given lest of points
+    print(coeffs)
+    polyprint(coeffs, desmos=desmos)
+    log("Calculating first derivative...")
+    derivative_1 = derivative(coeffs) # Calculate the first derivative
+    polyprint(derivative_1, desmos=desmos)
+    log("Calculating second derivative...")
+    derivative_2 = derivative(derivative_1) # Calculate the second derivative
+    polyprint(derivative_2, desmos=desmos)
+
+    # Graph the equations
+    log("Graphing equations...")
+    x_linspace = np.linspace(0, len(y_axis) - 1, len(y_axis) * 10 - 10)
     plt.scatter(x_axis, y_axis, color="black")
     plt.plot(x_linspace, polygraph(x_linspace, coeffs), color="red")
     plt.plot(x_linspace, polygraph(x_linspace, derivative_1), color="green")
     plt.plot(x_linspace, polygraph(x_linspace, derivative_2), color="blue")
+
+    log(f"Done in {math.floor(time() - ms)} ms.")
     plt.show()
